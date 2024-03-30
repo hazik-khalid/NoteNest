867d9f2467df55059bc875ad6f5ce549fc9ae33d
hazik <121675611+hazik-khalid@users.noreply.github.com>
2024-03-30 16:59:34 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,15 @@
 import { IonContent, IonPage } from "@ionic/react";
 import Navbar from "../components/Navbar";
-import Card from "../components/Card";
 import AddBtn from "../components/AddBtn";
-import { data } from "../components/AddBtn";
+// import { data } from "../components/AddBtn";
 
 const Home: React.FC = () => (
   <IonPage>
     <Navbar />
     <IonContent>
-      {data.map((card, index) => (
+      {/* {data.map((card, index) => (
         <Card key={index} title={card.title} text={card.text} />
-      ))}
+      ))} */}
 
       <AddBtn />
     </IonContent>
